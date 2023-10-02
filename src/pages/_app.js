bccf2897c1e47f8056b57139c6b77779f7c8bee6
salmanarrahman <salmanarrahman@gmail.com>
2023-10-02 23:14:44 +0600

    first

---
+++ src/pages/_app.js
@@ -0,0 +1,15 @@
+import { store } from "@/redux/store";
+import Footer from "@/shared/Footer";
+import Navbar from "@/shared/Navbar";
+import "@/styles/globals.css";
+import { Provider } from "react-redux";
+
+export default function App({ Component, pageProps }) {
+  return (
+    <Provider store={store}>
+      <Navbar />
+      <Component {...pageProps} />
+      <Footer />
+    </Provider>
+  );
+}
